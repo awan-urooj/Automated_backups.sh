965b5274db6a02fb2c28381e8ade95723aa6e297
Jay-Legend <38770386+Jay-Legend@users.noreply.github.com>
2019-11-12 11:38:54 -0500

    Add files via upload

---
+++ automated_backups.sh
@@ -0,0 +1,25 @@
+# This script will automate the back ups of specified folders and files of a user's home directory
+# It is the preperation before being sent to a remote server by the server team
+
+#This portion if for the zenity prompt
+#
+#
+#
+#
+#
+
+
+#This portion is for the the archiving and parsing
+#
+#
+#
+#
+#
+
+#This portion is for the logging
+#
+#
+#
+#
+#
+#
